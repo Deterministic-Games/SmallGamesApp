0d8f6061d08e34a870b735073e9fced5547457f3
Silop024 <jackmdwebb@hotmail.com>
2022-08-04 20:10:45 +0200

    Added commands for flagging and opening a square

---
+++ Minesweeper/Controls/SquareControl.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace Minesweeper.Controls;
+
+public class SquareControl : Button
+{
+    new public static readonly DependencyProperty CommandProperty = DependencyProperty.RegisterAttached("Command", typeof(ICommand), typeof(SquareControl));
+    [Bindable(true)]
+    [Category("Action")]
+    new public ICommand Command
+    {
+        get => (ICommand)GetValue(CommandProperty);
+        set => SetValue(CommandProperty, value);
+    }
+
+
+    public static readonly DependencyProperty Command2Property = DependencyProperty.RegisterAttached("Command2", typeof(ICommand), typeof(SquareControl));
+    [Bindable(true)]
+    [Category("Action")]
+    public ICommand Command2 
+    { 
+        get => (ICommand)GetValue(Command2Property); 
+        set => SetValue(Command2Property, value);
+    }
+
+    public SquareControl() : base() { }
+
+    protected override void OnMouseLeftButtonDown(MouseButtonEventArgs e)
+    {
+        if (Command.CanExecute(null)) Command.Execute(null);
+    }
+
+    protected override void OnMouseRightButtonDown(MouseButtonEventArgs e)
+    {
+        if (Command2.CanExecute(null)) Command2.Execute(null);
+    }
+
+    protected override void OnClick() { }
+}
--- Minesweeper/ViewModels/SquareViewModel.cs
+++ Minesweeper/ViewModels/SquareViewModel.cs
@@ -1,11 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Windows.Input;
+using Minesweeper.Commands;
+using Minesweeper.Models;
 
 namespace Minesweeper.ViewModels;
 
 public class SquareViewModel
 {
+    public SquareModel Square { get; init; }
+
+    public ICommand OpenCommand { get; init; }
+    public ICommand FlagCommand { get; init; }
+
+    public bool CanOpen => !(Square.IsOpened || Square.IsFlagged);
+    public bool CanFlag => !Square.IsOpened;
+
+    public SquareViewModel()
+    {
+        Square = new SquareModel();
+        OpenCommand = new OpenSquareCommand(this);
+        FlagCommand = new FlagSquareCommand(this);
+    }
+
+    public void Open()
+    {
+        Square.IsOpened = true;
+        Square.NeighbourMineCount = 6;
+    }
+
+    public void ToggleFlag()
+    {
+        Square.IsFlagged = !Square.IsFlagged;
+        Square.NeighbourMineCount = 1;
+    }
 }
