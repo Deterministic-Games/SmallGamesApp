c36e9a4b414039337fb90fe4edbe26374825c086
Silop024 <jackmdwebb@hotmail.com>
2022-08-04 20:10:26 +0200

    Implemented INotifyPropertyChanged

--- Minesweeper/Models/SquareModel.cs
+++ Minesweeper/Models/SquareModel.cs
@@ -1,15 +1,58 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
 
 namespace Minesweeper.Models;
 
-public struct SquareModel
+public class SquareModel : INotifyPropertyChanged
 {
-    public bool HasMine { get; set; }
-    public bool IsFlagged { get; set; }
-    public bool IsOpened { get; set; }
-    public byte NeighbourMineCount { get; set; }
+    #region Member variables
+    private bool _hasMine;
+    public bool HasMine 
+    { 
+        get { return _hasMine; }
+        set
+        {
+            _hasMine = value;
+            OnPropertyChanged();
+        }
+    }
+    private bool _isFlagged;
+    public bool IsFlagged 
+    {
+        get { return _isFlagged; }
+        set
+        {
+            _isFlagged = value;
+            OnPropertyChanged();
+        }
+    }
+    private bool _isOpened;
+    public bool IsOpened 
+    {
+        get { return _isOpened; }
+        set
+        {
+            _isOpened = value;
+            OnPropertyChanged();
+        }
+    }
+    private byte _neighbourMineCount;
+    public byte NeighbourMineCount 
+    {
+        get { return _neighbourMineCount; }
+        set
+        {
+            _neighbourMineCount = value;
+            OnPropertyChanged();
+        }
+    }
+    #endregion
+
+    #region Propety Changed logic
+    public event PropertyChangedEventHandler? PropertyChanged;
+    internal void OnPropertyChanged([CallerMemberName] string? name = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+    }
+    #endregion
 }
