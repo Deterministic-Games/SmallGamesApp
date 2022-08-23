c5bdc616e69fad98c1e7b3aa9e248747e71d9837
Silop024 <jackmdwebb@hotmail.com>
2022-08-23 17:44:32 +0200

    tictactoe view

---
+++ SmallGamesApp/TicTacToe/StateToContentConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using SmallGamesApp.MVVMToolkit.TicTacToe;
+
+namespace SmallGamesApp.TicTacToe;
+
+public class StateToContentConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is SquareState state)
+        {
+            return state switch
+            {
+                SquareState.Player1 => "X",
+                SquareState.Player2 => "O",
+                _ => " "
+            };
+        }
+        return " ";
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => throw new NotImplementedException();
+}
---
+++ SmallGamesApp/TicTacToe/TicTacToeView.xaml.cs
@@ -0,0 +1,14 @@
+﻿using SmallGamesApp.MVVMToolkit.TicTacToe;
+using System.Windows.Controls;
+
+namespace SmallGamesApp.TicTacToe;
+public partial class TicTacToeView : UserControl
+{
+    private TicTacToeBoardViewModel _viewModel;
+    public TicTacToeView()
+    {
+        InitializeComponent();
+        _viewModel = new TicTacToeBoardViewModel();
+        DataContext = _viewModel;
+    }
+}
