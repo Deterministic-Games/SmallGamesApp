e0c73e66662bea0d046ca5ef36e54f99b9f822af
Silop024 <jackmdwebb@hotmail.com>
2022-09-01 20:27:48 +0200

    Minor changes

---
+++ SmallGamesApp/Converters/BoolToVisibilityConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace SmallGamesApp;
+internal class BoolToVisibilityConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        var boolean = (bool)value;
+
+        return boolean switch
+        {
+            true => Visibility.Visible,
+            false => Visibility.Hidden
+        };
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => throw new NotImplementedException();
+}
