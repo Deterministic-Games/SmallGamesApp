52da1ea9bb1e1526d40bcc969c544da51ab70890
Silop024 <jackmdwebb@hotmail.com>
2023-03-17 18:08:14 +0100

    Create ConnectFourAI.cs

---
+++ SmallGamesApp.MVVMToolkit/ConnectFour/ConnectFourAI.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmallGamesApp.MVVMToolkit;
+
+public class ConnectFourAI
+{
+    private ConnectFourBoardVM _state;
+
+    public ConnectFourAI(ConnectFourBoardVM state)
+    {
+        _state = state.Copy();
+    }
+}
+
